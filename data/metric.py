b61617ad137c8156e0b59ce4e2510e3151783935
Chris Loew <cloew123@gmail.com>
2018-01-14 17:02:41 -0700

    Added placeholder metrics so we actually have data to play with

---
+++ data/metric.py
@@ -0,0 +1,8 @@
+
+class Metric:
+    """ Represents a Metric for calculating the Street Credit Score """
+    
+    def __init__(self, name, description):
+        """ Initialize with the name and description """
+        self.name = name
+        self.description = description
--- data/metrics_factory.py
+++ data/metrics_factory.py
@@ -1,9 +1,14 @@
+from .metric import Metric
 
 class MetricsFactory:
     """ Helper class to load the Existing Metrics """
     
     def load_all(self):
         """ Return all the available Metrics """
-        return []
+        return [
+            Metric("Prefers exactly 49 slices of pepperoni on a pizza.", "The perfect amount! (Proven by SCIENCE!)"),
+            Metric("Uses Facebook.", "Seriously?! You know who uses Facebook? Your parents. And their parents. And Grumpy Cat!"),
+            Metric("Saved a baby from an alligator by punching it in the face.", "We're gonna level with you. We have no idea if this happened, but it sounded AWESOME!!! And everybody's talking about so we figured we'd give it to ya."),
+        ]
         
 MetricsFactory = MetricsFactory()
