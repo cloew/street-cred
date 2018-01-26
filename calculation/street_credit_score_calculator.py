115daffb45c8607ba9bd8655178ed8e82e92cd5c
Chris Loew <cloew123@gmail.com>
2018-01-25 17:50:21 -0700

    Added baseline metric to give 300 points for being alive

--- calculation/street_credit_score_calculator.py
+++ calculation/street_credit_score_calculator.py
@@ -1,7 +1,10 @@
 from .metric_calculator import MetricCalculator
+from .metric_score import MetricScore
 from .street_credit_score import StreetCreditScore
 
-from data import MetricsFactory
+from data import Metric, MetricsFactory
+
+from cached_property import cached_property
 
 class StreetCreditScoreCalculator:
     """ Helper class to calculate a Person's Credit Score """
@@ -10,9 +13,15 @@ class StreetCreditScoreCalculator:
         """ Return the Street Credit Score """
         metrics = MetricsFactory.loadAll()
         metricCalculator = MetricCalculator()
-        scores = []
+        scores = [self.being_alive_metric]
         for metric in metrics:
             metric_score = metricCalculator.calculate(metric)
             scores.append(metric_score)
         return StreetCreditScore(scores)
         
+    @cached_property
+    def being_alive_metric(self):
+        """ Return the Being Alive Metric Score """
+        metric = Metric("Being alive.", "Congratulations on not being dead! This entitles you to 300 Street Cred!", calculator=None)
+        return MetricScore(metric, 300)
+    
--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,3 @@
 from .metrics_factory import MetricsFactory
+
+from .metric import Metric
