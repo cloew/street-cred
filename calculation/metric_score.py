2006db77b6a80a85a0928407f6e2f029fd4a8883
Chris Loew <cloew123@gmail.com>
2018-02-05 11:04:11 -0700

    Fixed naming scheme to be consistent (darn snake case)

--- calculation/metric_score.py
+++ calculation/metric_score.py
@@ -1,7 +1,7 @@
 from .metric_environment import MetricEnvironment
 from proxy_attrs import proxy_for
 
-@proxy_for('scoreContext', ['score'])
+@proxy_for('score_context', ['score'])
 class MetricScore:
     """ Represents a particular Metric's Score """
     
@@ -10,17 +10,17 @@ class MetricScore:
         """ Build a Metric Score from the given Metric """
         return cls(metric, metric.calculate())
     
-    def __init__(self, metric, scoreContext):
+    def __init__(self, metric, score_context):
         """ Initialize with the metric and score """
         self.metric = metric
-        self.scoreContext = scoreContext
+        self.score_context = score_context
         
     @property
     def name(self):
         """ The name for this Metric """
-        return MetricEnvironment.render(self.metric.name, self.scoreContext)
+        return MetricEnvironment.render(self.metric.name, self.score_context)
         
     @property
     def description(self):
         """ The description for this Metric """
-        return MetricEnvironment.render(self.metric.description, self.scoreContext)
+        return MetricEnvironment.render(self.metric.description, self.score_context)
